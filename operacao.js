66ef0282d4bed683c0a795bb76341bd7cb81010a
MatheusLaiaa <144149403+MatheusLaiaa@users.noreply.github.com>
2024-03-30 15:47:43 -0300

    Add files via upload

---
+++ operacao.js
@@ -0,0 +1,15 @@
+
+var num1 = 20;
+var num2 = 20;
+
+
+var soma = num1 + num2;
+var subtracao = num1 - num2;
+var multiplicacao = num1 * num2;
+var divisao = num1 / num2;
+
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
+
